38547b5aa32aac42ecb8580dc6b84502a01e72db
Robert <robertruben98@gmail.com>
2024-08-12 11:59:22 -0400

    TEST

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hello, I'm Jenn</h1>
+        <h1 className={styles.title}>Hello, I'm Jenn, Robert girlfriend</h1>
         <p className={styles.description}>
             I have over 3 years of experience in frontend development. I'm skilled in creating user interfaces and enhancing user experiences. Reach out if you'd like to learn more!
         </p>

0ac34e18f5fa8683f4093cbf907c6c40c6ce1bca
pushita99 <pushita990301@gmail.com>
2024-08-13 17:30:28 +0200

    remove proys for the moment until i have something awsome

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import styles from "./App.module.css";
-import { Navbar } from "./components/Navbar/Navbar";
-import { Hero } from "./components/Hero/Hero";
-import { About } from "./components/About/About";
-import { Experience } from "./components/Experience/Experience";
-import { Contact } from "./components/Contact/Contact";
+import {Navbar} from "./components/Navbar/Navbar";
+import {Hero} from "./components/Hero/Hero";
+import {About} from "./components/About/About";
+import {Experience} from "./components/Experience/Experience";
+import {Contact} from "./components/Contact/Contact";
 import {Projects} from "./components/Projects/Projects.jsx";
 import {LandingPage} from "./components/Componens/LandingPage/LandingPage.jsx";
 import {QuizApp} from "./components/Componens/QuizApp/QuizApp.jsx";
@@ -16,11 +16,11 @@ import Store from "./components/Componens/Store/Store.jsx";
 function Home() {
     return (
         <>
-            <Hero />
-            <About />
-            <Experience />
-            <Projects />
-            <Contact />
+            <Hero/>
+            <About/>
+            <Experience/>
+           {/* <Projects/>*/}
+            <Contact/>
         </>
     );
 }
@@ -29,15 +29,15 @@ function App() {
     return (
         <Router>
             <div className={styles.App}>
-                <Navbar />
+                <Navbar/>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/landing-page-demo" element={<LandingPage />} />
-                    <Route path="/quiz-app" element={<QuizApp />} />
-                    <Route path="/quiz-app" element={<QuizApp />} />
-                    <Route path="/admin-dashboard" element={<Dashboard />} />
-                    <Route path="/store" element={<Store />} />
-                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/landing-page-demo" element={<LandingPage/>}/>
+                    <Route path="/quiz-app" element={<QuizApp/>}/>
+                    <Route path="/quiz-app" element={<QuizApp/>}/>
+                    <Route path="/admin-dashboard" element={<Dashboard/>}/>
+                    <Route path="/store" element={<Store/>}/>
+                    <Route path="/checkout" element={<Checkout/>}/>
                 </Routes>
             </div>
         </Router>
